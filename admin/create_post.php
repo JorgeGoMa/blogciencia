71faf6dfe420e8d7125fa463607995a3078ec4e0
Francisco Jasso <pacojare_0223@hotmail.com>
2021-01-18 23:16:34 -0600

    arreglandoerror

--- admin/create_post.php
+++ admin/create_post.php
@@ -1,6 +1,7 @@
 <?php  include('../config.php'); ?>
 <?php  include(ROOT_PATH . '/admin/includes/admin_functions.php'); ?>
 <?php  include(ROOT_PATH . '/admin/includes/post_functions.php'); ?>
+<?php  include(ROOT_PATH . '/admin/includes/topics_functions.php'); ?>
 <?php include(ROOT_PATH . '/admin/includes/header.php'); ?>
 <!-- Obtener las Etiquetas -->
 <?php $topics = getAllTopics();	?>
